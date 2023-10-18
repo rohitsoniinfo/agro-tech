9b91612fd7093642234c900dc414c432927c92c8
Aditi <adithakurti@gmail.com>
2023-10-18 09:57:12 +0530

    Added Weather Screen

--- app/lib/Screens/weatherScreen.dart
+++ app/lib/Screens/weatherScreen.dart
@@ -7,10 +7,77 @@ class weatherScreen extends StatefulWidget {
 class _weatherScreenState extends State<weatherScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'Weather Screen.',
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Location'),
+            Row(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {
+                    // Handle search button press
+                  },
+                ),
+                PopupMenuButton<String>(
+                  icon: Icon(Icons.more_vert),
+                  onSelected: (value) {
+                    // Handle popup menu item selection
+                  },
+                  itemBuilder: (BuildContext context) {
+                    return ['Settings', 'About', 'Exit'].map((String choice) {
+                      return PopupMenuItem<String>(
+                        value: choice,
+                        child: Text(choice),
+                      );
+                    }).toList();
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'City Name',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Date and Time',
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+            SizedBox(height: 16),
+            Row(
+              children: [
+                Icon(Icons.cloud_outlined, size: 64,
+                ),
+                SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Temperature: 25°C',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Extra Information about Cloud',
+                      style: TextStyle(fontSize: 16, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
